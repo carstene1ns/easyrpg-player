303670de52e696fb9ad7c00a8a8ab103579f0314
Carsten Teibes <dev@f4ke.de>
2017-05-27 02:56:01 +0200

    Cleanup

--- builds/android/app/src/main/jni/gamebrowser/Android.mk
+++ builds/android/app/src/main/jni/gamebrowser/Android.mk
@@ -1,22 +1,22 @@
 LOCAL_PATH := $(call my-dir)
 
+EASYRPG_TOOLCHAIN_DIR = $(EASYDEV_ANDROID)/$(TARGET_ARCH_ABI)-toolchain
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := gamebrowser
 
-EASYRPG_TOOLCHAIN_DIR = $(EASYDEV_ANDROID)/$(TARGET_ARCH_ABI)-toolchain
-
-LOCAL_C_INCLUDES := \
-	$(EASYRPG_TOOLCHAIN_DIR)/include \
-	$(EASYRPG_TOOLCHAIN_DIR)/include/libpng16
+LOCAL_C_INCLUDES :=
 
 LOCAL_SRC_FILES := \
 	org_easyrpg_player_game_browser_GameScanner.cpp
 
-LOCAL_LDLIBS := -L$(EASYRPG_TOOLCHAIN_DIR)/lib -llog -lz -lpng
+LOCAL_LDLIBS := -llog -lz
 
-LOCAL_CFLAGS := -O2 -Wall -Wextra -fno-rtti
+LOCAL_STATIC_LIBRARIES := png
 
-LOCAL_CPPFLAGS = $(LOCAL_C_FLAGS) -std=c++11
+LOCAL_CFLAGS := -O2 -Wall -Wextra
+
+LOCAL_CXXFLAGS := $(LOCAL_C_FLAGS) -std=c++11 -fno-rtti
 
 include $(BUILD_SHARED_LIBRARY)
